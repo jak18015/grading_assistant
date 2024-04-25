927e5c71e2e0182c5ff4375c7d189854266580e2
JKToxo <104020305+jak18015@users.noreply.github.com>
2024-04-25 09:12:34 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
+import time
+import json
+from statistics import mean
 from assets import GradingAssistant, rubric
 
 
 def main():
-    rubric_data = rubric("assets/rubric.json")
-    grade = GradingAssistant(rubric_data)
-    grade.grade()
-    grade.summarize()
+    time_start = time.time()
 
+    rubric_data = rubric("assets/sep_of_proteins_2_3.json")
+    assistant = GradingAssistant(rubric_data)
+    assistant.iterate_over_dictionary(assistant.rubric_dict)
+    assistant.summarize()
+    
+    time_end = time.time()
+    duration = round((time_end - time_start)/60, 1)
+    print(f'\n{duration} minutes')
+    
+    with open('time_log.json', mode='r', encoding='utf-8') as time_file:
+        time_log: list = json.load(time_file)
+        time_log.append(duration)
+    with open('time_log.json', mode='w', encoding='utf-8') as time_file:
+        json.dump(time_log, time_file, indent=4)
+
+    print(f'Avg time: {round(mean(time_log), 1)} minutes')
+    # assistant.debug()
     
 if __name__ == "__main__":
     main()
